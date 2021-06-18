54e31e701fd516e39a303ef6b88876b43887ae21
CARNEIRO BARBOSA Fábio (EXT) <fabio.barbosa.ext@eurocontrol.int>
2021-06-18 11:27:10 +0200

    today's talk was excellent

--- R/list_apt_files.R
+++ R/list_apt_files.R
@@ -3,8 +3,8 @@
 # .year is a 4-digit year, set "*" for all
 # . opr is either "DEP" for departures or "ARR" for arrivals/landings
 
-list_apt_files <- function(.aprt = "*", .year = "*", .opr = "*", .path = "./data-raw/"){
-  ptn <- paste0(.aprt, ".*.", .year, ".*.", .opr)
+list_apt_files <- function(.year = "*", .path = "./data-raw/"){
+  ptn <- paste0("flightlist_*.", .year, ".*.")
   fns <- list.files(.path, ptn)
   full_fns <- c(paste0(.path, fns))
   return(full_fns)
